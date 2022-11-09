c4640889ddbfa43f27db625bcf45e9b531af4275
Armando <armando.gonzalez@correo.unimet.edu.ve>
2022-11-08 21:55:10 -0600

    Finish excercise

--- Main.py
+++ Main.py
@@ -5,25 +5,34 @@ Issue: piedra, papel o tijera
     1- mostrar los tipos de elementos en el output ✔️ 
     2- mostrar los tipos de elementos en el output (Computadora) ✔️
     3- comparar los resultados user vs pc ✔️
+    4- el sistema se para cuando se presiona [4] ✔️
 
 """
 
 def main():
     
-    chooseUser = input('\n 1-piedra:\n 2-papel:\n 3-tijera:\n');
-    
-    if chooseUser.isnumeric():
-        if int(chooseUser) in range(1,4):
-            chooseUserString = IsRockSisorsPaper(int(chooseUser));
-            choosePcString = IsRockSisorsPaper();
+    chooseUser = input('\n 1-piedra:\n 2-papel:\n 3-tijera:\n 4-salir:\n');
+    while True:
+        if chooseUser.isnumeric():
+            if int(chooseUser) in range(1,4):
+                
+                chooseUserString = IsRockSisorsPaper(int(chooseUser));
+                choosePcString = IsRockSisorsPaper();
 
-            print("User choose: " + chooseUserString);
-            print("PC choose:" + choosePcString);
-            Compare(chooseUserString, choosePcString);
+                print("User choose: " + chooseUserString);
+                print("PC choose:" + choosePcString);
+                Compare(chooseUserString, choosePcString);
+                chooseUser = input('\n 1-piedra:\n 2-papel:\n 3-tijera:\n 4-salir:\n');
 
+            elif int(chooseUser) == 4:
+                break;
+            else:
+                print("No está en las opciones requeridas");
+                chooseUser = input('\n 1-piedra:\n 2-papel:\n 3-tijera:\n 4-salir:\n');
         else:
-            print("NO está en el rango, BICHO DE UÑA");
-    else:
-        print(False);
+           print("No está en las opciones requeridas");
+           chooseUser = input('\n 1-piedra:\n 2-papel:\n 3-tijera:\n 4-salir:\n');
+
+    
 
 main();
